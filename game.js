10884172469c91a23305b5a1e37273366e3649ff
Zoya <Zoya@DESKTOP-7HMVPJQ>
2016-07-30 12:52:39 -0400

    Smaller, faster, more accurate aim.

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 ﻿var area = document.getElementById("area");
 var target = document.getElementById("target");
+var targetX, targetY;
 var aim = document.getElementById("aim");
 var areaW = 500, areaH = 720;
 var targetW = 145, targetH = 90; //fly.gif aspect ratio is 1.605 width/height
-var aimW = 50, aimH = 50, aimL = 0, aimT = 0;
-var targetSpeed = 3000;
+var aimW = 20, aimH = 20, aimL = 0, aimT = 0;
+var speed = 10000;
+var targetSpeed = speed;
 var score = 0;
 var showScore = document.getElementById("score");
 var showLevel = document.getElementById("level");
@@ -14,27 +16,18 @@ var changeLevelAfterScore = 3;
 var moveInterval;
 
 
-var canvas = document.createElement("canvas");
-canvas.width = 500;
-canvas.height = 720;
-area.appendChild(canvas);
-var ctx = canvas.getContext("2d");
-canvas.style.marginLeft= "auto";
-canvas.style.marginRight = "auto";
-canvas.style.position = "absolute";
-canvas.style.top = "0";
 
 
 function placeTarget() {
     clearInterval(moveInterval);
     var maxX = areaW - targetW; var maxY = areaH - targetH;
-    var targetX, targetY;
+    
     targetX = Math.floor(Math.random() * (maxX + 1));
     targetY = Math.floor(Math.random() * (maxY + 1));
     target.style.top = targetY + "px";
     target.style.left = targetX + "px";
     target.addEventListener("click", scoreUp, false);//ONLY if successful click then goes to scoreUp.
-
+    addEventListener("keydown", keyEvents, false);//for keyboard arrowkeys
 
     moveInterval = setInterval(placeTarget, targetSpeed);
 
@@ -73,19 +66,18 @@ function showFirework()
     //targetEle.style.backgroundImage = "url(splash.gif)";
     //http://gifduration.konstochvanligasaker.se/ to see duration of firework.GIF w is 2480 millisec. 
     setTimeout(function () { moveInterval = setInterval(placeTarget, targetSpeed); loadSelectedImg(); placeTarget(); }, 1600);
-    
 }
 
 function speedReset()
 {
-    targetSpeed = 3000;
+    targetSpeed = speed;
     level = 1; showLevel.innerHTML = "Level: " + level;
     startTarget();
 }
 
 function gameReset()
 {
-    targetSpeed = 3000;
+    targetSpeed = speed;
     score = 0; showScore.innerHTML = "Score: " + score;
     level = 1; showLevel.innerHTML = "Level: " + level;
     startTarget();
@@ -111,6 +103,7 @@ function startGame()
     placeTarget();
     drawAim();
 }
+
 function drawAim() {
     aim.style.width = aimW + "px";
     aim.style.height = aimH + "px";
@@ -135,58 +128,83 @@ function loadSelectedImg()
 
 
 
-addEventListener("keydown", function (e) //same as window.addEventListener. Moves the aim.
+var keyEvents = function (e) 
 {
-    e.preventDefault(); //prevent down arrow from scrolling scroll-bar.
+    //e.preventDefault(); //here it causes ALL keys form working properly, like ctrl+F5 does refresh the page!!
     var speed = 10;
 
     if (e.keyCode == 40 && //down arrow
         aimT <= (areaH - speed - aimH)) //areaH-speed so that increenting stops when ain get to edges. -ainH so that aim doesn't go out of the area.
     {
+        e.preventDefault(); //prevent down arrow from scrolling scroll-bar.
         drawAim(); //re-draw aim
         aimT = aimT + speed; //move aim
     }
     if (e.keyCode == 38 && aimT>=speed)//up arrow
     {
+        e.preventDefault();
         drawAim(); 
         aimT = aimT - speed;
     }
     if (e.keyCode == 37 && aimL>=speed)//left arrow
     {
+        e.preventDefault();
         drawAim(); 
         aimL = aimL - speed;
     }
     if (e.keyCode == 39 && aimL<= (areaW-speed-aimW))//right arrow
     {
+        e.preventDefault();
         drawAim(); 
         aimL = aimL + speed;
     }
-}, false);
+    checkContact();
+}
 
+
+function checkContact() //see if aim & target have come in contact AFTER moving aim e keys. 
+{
+    if(aimL+aimW-20 > targetX  && targetX+targetW-20 > aimL  && aimT+aimH > targetY  && targetY+targetH > aimT)
+    {
+        scoreUp();
+        removeEventListener("keydown", keyEvents, false);
+    }
+}
 
 window.addEventListener("load", startTarget, false);
 document.getElementById("btnSpeedReset").addEventListener("click", speedReset ,false);
 document.getElementById("btnGameReset").addEventListener("click", gameReset, false);
-
 document.getElementById("startGame").addEventListener("click", startGame, false);
+addEventListener("keydown", keyEvents, false);//same as window.addEventListener. Moves the aim.
 
 
 
 
 
 
+//var canvas = document.createElement("canvas");
+//canvas.width = 500;
+//canvas.height = 720;
+////area.appendChild(canvas);
+//document.body.appendChild(canvas);
+//var ctx = canvas.getContext("2d");
+//canvas.style.marginLeft= "auto";
+//canvas.style.marginRight = "auto";
+////canvas.style.position = "absolute";
+////canvas.style.top = "0";
+////canvas.style.zIndex = "9";
 
+//var aimImgReady = false;
+//var aimImg = new Image();
+//aimImg.onload = function () { aimImgReady = true;}
+//aimImg.src = "aim.png";
 
-
-
-
-
-
-
-
-
-
-
+//function drawAimImg()
+//{
+//    if(aimImgReady)
+//        ctx.drawImage(aimImg, 100, 100);
+//}
+//requestAnimationFrame(drawAimImg);
 
 
 
